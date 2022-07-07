72ef9cd02dba7d61e61bd163911d982257b8d81d
Pankaj yadav <Pankajy@intigate.in>
2022-07-07 12:01:21 +0530

    loader is done on home api load

--- src/ApiCallProducts.jsx
+++ src/ApiCallProducts.jsx
@@ -4,13 +4,16 @@ import "./Pankaj.css";
 import "./App.css";
 import placeholder from "./starlogo.png";
 import ProgressiveImg from "./progressiveImage/ProgressiveImg";
+import { TbLoader } from "react-icons/tb";
 
 const Pankaj = () => {
   const [first, setfirst] = useState([]);
+  const [loader, setLoader] = useState(false);
 
   const fetchApi = async () => {
     const response = await axios.get("https://fakestoreapi.com/products");
     setfirst(response.data);
+    setLoader(true);
     console.log(response.data).catch((err) => {
       console.log("Error", err);
     });
@@ -22,22 +25,27 @@ const Pankaj = () => {
 
   return (
     <div className="all-cards">
-      {first.map((data, id) => {
-        const { title, image, category } = data;
-        return (
-          <div className="card" key={id}>
-            {/* <img/> */}
-            <ProgressiveImg
-              custom_class="mycard-img"
-              src={image}
-              placeholder={placeholder}
-              alt={title}
-            />
-            <h1 className="card-title">{title}</h1>
-            <p> {category} </p>
-          </div>
-        );
-      })}
+      {loader ? (
+        first.map((data, id) => {
+          const { title, image, category } = data;
+          return (
+            <div className="card" key={id}>
+              <ProgressiveImg
+                custom_class="mycard-img"
+                src={image}
+                placeholder={placeholder}
+                alt={title}
+              />
+              <h1 className="card-title">{title}</h1>
+              <p> {category} </p>
+            </div>
+          );
+        })
+      ) : (
+        <div className="loader-div">
+          <TbLoader className="loader-icon" />
+        </div>
+      )}
     </div>
   );
 };
